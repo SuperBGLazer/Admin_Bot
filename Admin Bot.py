341897c730ff4d9e4ca2d6ba02cc7e000c31d697
SuperBGLazer <superlazer@icloud.com>
2019-09-07 10:34:25 -0500

    You can now do more then one ^ command at the same time.

--- Admin Bot.py
+++ Admin Bot.py
@@ -13,9 +13,6 @@ def read_token():
 client = discord.Client()
 
 
-
-
-
 @client.event
 async def on_member_join(member):
     global joined
@@ -26,31 +23,39 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    id = client.get_guild(592447508219953163)
-    if message.content.find("^hello") != -1:
-        await message.channel.send(f"Hello {message.author}")
-        print(f" {message.author} Said Hello")
-    elif message.content == "^users":
-        await message.channel.send(f" Number of Members: {id.member_count}")
-        print(f"""{message.author} Asked for users""")
-    elif message.content == "^bot":
-        await  message.channel.send(f"This is are GitHub Bot https://github.com/Gunn1/Admin_Bot")
-        print(f"{message.author} Just typed GitHub")
-    elif message.content == "^help":
-        embed = discord.Embed(title="Admin Bot", description="Some useful commands")
-        embed.add_field(name="^hello", value="Greets the user")
-        embed.add_field(name="^users", value="Prints number of users")
-        embed.add_field(name="^welcome", value="Welcome People")
-        embed.add_field(name="GitHub", value="GitHub bot")
-        await message.channel.send(content=None, embed=embed)
-    elif message.content == "^welcome":
-        await message.channel.send("Welcome All")
-    elif "^" in message.content:
-        await message.channel.send("Sorry I do not know that.")
+    # id = client.get_guild(592447508219953163)
+    id = client.get_guild(os.environ['CLIENT_ID'])
+    if "^" in message.content:
+        found = False
+        if "^hello" in message.content:
+            await message.channel.send(f"Hello {message.author}")
+            print(f" {message.author} Said Hello")
+            found = True
+        if "^users" in message.content:
+            await message.channel.send(f" Number of Members: {id.member_count}")
+            print(f"""{message.author} Asked for users""")
+            if message.content == "^bot":
+                await  message.channel.send(f"Here is my source code https://github.com/Gunn1/Admin_Bot")
+            print(f"{message.author} Just typed GitHub")
+            found = True
+        if "^help" in message.content:
+            embed = discord.Embed(title="Admin Bot", description="Some useful commands")
+            embed.add_field(name="^hello", value="Greets the user")
+            embed.add_field(name="^users", value="Prints number of users")
+            embed.add_field(name="^welcome", value="Welcome People")
+            embed.add_field(name="^bot", value="GitHub bot source code")
+            found = True
+            await message.channel.send(content=None, embed=embed)
+        if "^welcome" in message.content:
+            await message.channel.send("Welcome All")
+            found = True
 
-
-
-
+        if not found:
+            # This will check to see if the ^ is before some text
+            pos = message.content.find("^")
+            if pos != -1:
+                if message.content[pos + 1] != " ":
+                    await message.channel.send("Sorry I do not know that.")
 
 
 print("Connected To Discord!")
