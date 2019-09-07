08f9b4bc6be453a1ab4f0c8238a46d83d7f0827b
Tyler <tylerjgunn@gmail.com>
2019-09-06 21:16:37 -0500

    Chat bot

--- Admin Bot.py
+++ Admin Bot.py
@@ -27,22 +27,29 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
     id = client.get_guild(592447508219953163)
-    if message.content.find("!hello") != -1:
+    if message.content.find("^hello") != -1:
         message.author == message.author
         await message.channel.send(f"Hello {message.author}")
         print(f" {message.author} Said Hello")
-    elif message.content == "!users":
+    elif message.content == "^users":
         await message.channel.send(f" Number of Members: {id.member_count}")
         print(f"""{message.author} Asked for users""")
     elif message.content == "GitHub":
         await  message.channel.send(f"This is are GitHub Bot https://github.com/Gunn1/Admin_Bot")
-    ...
-    if message.content == "!help":
+        print(f"{message.author} Just typed GitHub")
+
+    elif message.content == "^help":
         embed = discord.Embed(title="Admin Bot", description="Some useful commands")
-        embed.add_field(name="!hello", value="Greets the user")
-        embed.add_field(name="!users", value="Prints number of users")
+        embed.add_field(name="^hello", value="Greets the user")
+        embed.add_field(name="^users", value="Prints number of users")
         await message.channel.send(content=None, embed=embed)
-    ...
+
+    elif "^" in message.content:
+        await message.channel.send("Sorry I do not now that.")
+
+
+
+
 
 print("Connected To Discord!")
 client.run(client.run(os.environ['TOKEN']))
