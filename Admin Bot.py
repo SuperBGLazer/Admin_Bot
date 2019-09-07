7b9e667aab93caf27761d2e86943f0ad81fee621
Tyler <tylerjgunn@gmail.com>
2019-09-06 19:52:42 -0500

    Chat bot

--- Admin Bot.py
+++ Admin Bot.py
@@ -48,11 +48,12 @@ async def on_member_join(member):
 async def on_message(message):
     id = client.get_guild(592447508219953163)
     if message.content.find("!hello") != -1:
-        print("User Said Hello")
-        await message.channel.send(f"Hello {message.auther}")
+        message.author == message.author
+        await message.channel.send(f"Hello {message.author}")
+        print(f" {message.author} Said Hello")
     elif message.content == "!users":
         await message.channel.send(f" Number of Members: {id.member_count}")
-        print(f""" Asked for !user""")
+        print(f"""{message.author} Asked for users""")
     ...
     if message.content == "!help":
         embed = discord.Embed(title="Admin Bot", description="Some useful commands")
