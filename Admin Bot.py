3994ff1f224b97e86087be547be8aa49bc567fef
Tyler <tylerjgunn@gmail.com>
2019-09-08 15:14:56 -0500

    Chat bot

--- Admin Bot.py
+++ Admin Bot.py
@@ -33,8 +33,8 @@ async def on_message(message):
         if "^users" in message.content:
             await message.channel.send(f" Number of Members: {id.member_count}")
             print(f"""{message.author} Asked for users""")
-            if message.content == "^bot":
-                await message.channel.send(f"Here is my source code https://github.com/Gunn1/Admin_Bot")
+        if message.content == "^bot":
+            await message.channel.send(f"Here is my source code https://github.com/Gunn1/Admin_Bot")
             print(f"{message.author} Just typed GitHub")
             found = True
         if "^help" in message.content:
